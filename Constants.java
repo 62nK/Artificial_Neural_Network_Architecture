a399ff0141b72771b8f01c1b8231ec2666be1a3d
andreapinardi <andreapinardi39@gmail.com>
2019-04-24 11:10:04 -0400

    Add files via upload

--- Constants.java
+++ Constants.java
@@ -2,26 +2,26 @@ public interface Constants {
 
     /** Neural Network **/
     // Neuron Configuration
-    int LAYER_COUNT = 3;
+    int LAYER_COUNT = 5;
     int INPUT_LAYER_NEURON_COUNT = 4;
-    int HIDDEN_LAYER_NEURON_COUNT = 8;
+    int HIDDEN_LAYER_NEURON_COUNT = 2;
     int OUTPUT_LAYER_NEURON_COUNT = 2;
 
     /** FLAGS **/
-    // Functioning Mode
-    boolean TRAINING_MODE = false;
-    boolean CATEGORIZING_MODE = !TRAINING_MODE;
-    // IO
-    boolean RESTORE = true; // Restore
-    boolean SAVE = TRAINING_MODE;  // Save final state of synapses
     // Debug: std output messages
     boolean DEBUG = true;
 
     // Training
-    int EPOCHS = 10000;
-    double LEARNING_RATE = 0.1; // -1 for learning rate = 1/(1+epoch#)
+    int EPOCHS = 500;
+    double LEARNING_RATE = 0.7; // -1 for learning rate = 1/(1+epoch#)
+    double MOMENTUM = 0.4;
 
     // IO
     String PATH = "src/cfg/";
-    String FILENAME = "ANNconfig.cfg";
+    String BACKUP = "backup.ann";
+    String FILENAME = LAYER_COUNT+"L"+INPUT_LAYER_NEURON_COUNT+"I"+HIDDEN_LAYER_NEURON_COUNT+"H"+OUTPUT_LAYER_NEURON_COUNT+"O.ann";
+
+    // JavaFX
+    double HEIGHT = 350;
+    double WIDTH = 200;
 }
--- TestANN.java
+++ TestANN.java
@@ -1,13 +1,38 @@
-import architecture.NeuralNetwork;
-import architecture.InputTargetPair;
+import ArtificialNeuralNetwork.NeuralNetwork;
+import ArtificialNeuralNetwork.InputTargetPair;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
 
-public class TestANN implements Constants {
+public class TestANN extends Application implements Constants {
 
-    public static void main(String[] args) {
+    // Variables
+    int layerCount = LAYER_COUNT;
+    int inputLayerNeuronCount = INPUT_LAYER_NEURON_COUNT;
+    int hiddenLayerNeuronCount = HIDDEN_LAYER_NEURON_COUNT;
+    int outputLayerNeuronCount = OUTPUT_LAYER_NEURON_COUNT;
+    int epochs = EPOCHS;
+    double learningRate = LEARNING_RATE;
+    double momentum = MOMENTUM;
+
+    String filename = PATH+FILENAME;
+
+    @Override
+    public void start(Stage primaryStage) {
 
         // Objects
         NeuralNetwork artificialNeuralNetwork;
+        BorderPane borderPane = new BorderPane();
 
         // Input
         ArrayList<InputTargetPair> trainingSet = new ArrayList<>();
@@ -40,44 +65,126 @@ public class TestANN implements Constants {
 
         }
         ArrayList<double[]> inputs = new ArrayList<>();
-        {
-            inputs.add(new double[]{-10, -10, -10, -10});
-            inputs.add(new double[]{-10, 10, -10, 10});
-            inputs.add(new double[]{10, 10, 10, 10});
-        }
+        for(InputTargetPair itp: trainingSet) inputs.add(itp.getInput());
 
-        // Restore Neurons
-        if(RESTORE) artificialNeuralNetwork = new NeuralNetwork(NeuralNetwork.readANNFromFile(PATH+FILENAME));
-        else artificialNeuralNetwork = new NeuralNetwork(LAYER_COUNT, INPUT_LAYER_NEURON_COUNT, HIDDEN_LAYER_NEURON_COUNT, OUTPUT_LAYER_NEURON_COUNT);;
-        if(DEBUG) System.out.println(artificialNeuralNetwork.toString("Architecture"));
+        artificialNeuralNetwork = new NeuralNetwork(layerCount, inputLayerNeuronCount, hiddenLayerNeuronCount, outputLayerNeuronCount);
+        // Output
+        if(DEBUG) print(artificialNeuralNetwork);
 
-        // Train
-        if(TRAINING_MODE) {
-            artificialNeuralNetwork.train(EPOCHS, LEARNING_RATE, trainingSet);
-            if(DEBUG) System.out.println("Training set error("+trainingSet.size()+"):"+artificialNeuralNetwork.getNetworkError());
-            artificialNeuralNetwork.test(testSet);
-            if(DEBUG) System.out.println("Test set error("+testSet.size()+"):"+artificialNeuralNetwork.getNetworkError());
-        }
+        // TextFields
+        HBox hBoxEpochs = new HBox();
+        Text textEpochs = new Text(0,0, "Epochs");
+        TextField textFieldEpochs = new TextField(""+epochs);
+        textFieldEpochs.setMaxWidth(60);
+        textFieldEpochs.textProperty().addListener(e -> {
+            int value = Integer.parseInt(textFieldEpochs.getText());
+            if(value>=0)
+                epochs = value;
+            if(DEBUG) System.out.println("Epochs value changed to: "+epochs);
+        });
+        hBoxEpochs.setAlignment(Pos.CENTER_LEFT);
+        hBoxEpochs.setSpacing(10);
+        hBoxEpochs.getChildren().add(textEpochs);
+        hBoxEpochs.getChildren().add(textFieldEpochs);
 
-        // Categorize
-        if(CATEGORIZING_MODE) {
-            for(double[] input: inputs){
-                artificialNeuralNetwork.feedForward(input);
-                double[] output = artificialNeuralNetwork.getOutput();
-                if(DEBUG) {
-                    System.out.println(artificialNeuralNetwork.outputToString());
-                    if (output[0] > output[1])
-                        System.out.println("Bright");
-                    else
-                        System.out.println("Dark");
-                }
-            }
-        }
+        HBox hbLearningRate = new HBox();
+        Text textLearningRate = new Text(0,0, "Learning Rate");
+        TextField textFieldLearningRate = new TextField(""+learningRate);
+        textFieldLearningRate.setMaxWidth(50);
+        textFieldLearningRate.textProperty().addListener(e -> {
+            double value = Double.parseDouble(textFieldLearningRate.getText());
+            if(value<=1 && value>=0 || value==-1)
+                learningRate = value;
+            if(DEBUG) System.out.println("learning rate value changed to: "+learningRate);
+        });
+        hbLearningRate.setAlignment(Pos.CENTER_LEFT);
+        hbLearningRate.setSpacing(10);
+        hbLearningRate.getChildren().add(textLearningRate);
+        hbLearningRate.getChildren().add(textFieldLearningRate);
 
-        // Save Neural Network
-        if(SAVE) artificialNeuralNetwork.writeANNToFile(PATH+FILENAME);
-        if(DEBUG) System.out.println(artificialNeuralNetwork.toString("Architecture"));
+        HBox hbMomentum = new HBox();
+        Text textMomentum = new Text(0,0, "Momentum");
+        TextField textFieldMomentum = new TextField(""+momentum);
+        textFieldMomentum.setMaxWidth(50);
+        textFieldMomentum.textProperty().addListener(e -> {
+            double value = Double.parseDouble(textFieldMomentum.getText());
+            if(value<=1 && value>=0)
+                momentum = value;
+            if(DEBUG) System.out.println("momentum value changed to: "+momentum);
+        });
+        hbMomentum.setAlignment(Pos.CENTER_LEFT);
+        hbMomentum.setSpacing(10);
+        hbMomentum.getChildren().add(textMomentum);
+        hbMomentum.getChildren().add(textFieldMomentum);
 
+        // Buttons
+        Button nextState = new Button("Next Example");
+        Button train = new Button("Train (Back Propagation)");
+        Button test = new Button("Test Set");
+        Button reset = new Button("Reset Synapses");
+        Button restore = new Button("Import ANN");
+        Button save = new Button("Export ANN");
+        VBox controlPanel = new VBox();
+        controlPanel.setAlignment(Pos.TOP_CENTER);
+        controlPanel.setSpacing(5);
+        controlPanel.setPadding(new Insets(5));
+        controlPanel.getChildren().addAll(hBoxEpochs, hbLearningRate, hbMomentum,
+                restore, save, reset, train, test, nextState);
+        nextState.setMinHeight(40);
+        borderPane.setLeft(controlPanel);
+        nextState.setOnMouseClicked(event -> {
+            categorize(artificialNeuralNetwork, new double[] {});
+        });
+        train.setOnMouseClicked(event -> {
+            train(artificialNeuralNetwork, trainingSet, epochs, learningRate, momentum);
+        });
+        test.setOnMouseClicked(event -> {
+            test(artificialNeuralNetwork, testSet);
+        });
+        reset.setOnMouseClicked(event -> {
+            reset(artificialNeuralNetwork);
+        });
+        restore.setOnMouseClicked(event -> {
+            read(artificialNeuralNetwork, filename);
+        });
+        save.setOnMouseClicked(event -> {
+            write(artificialNeuralNetwork, filename);
+        });
+
+        // Put the pane on the scene and the scene on the stage
+        Scene scene = new Scene(borderPane, WIDTH, Constants.HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Artificial Neural Network Dark/Bright detection with Simulated Annealing - Andrea Pinardi");
+        primaryStage.show();
+    }
+
+    public static void reset(NeuralNetwork neuralNetwork){
+        neuralNetwork.resetSynapses();
+        if(DEBUG) print(neuralNetwork);
+    }
+    public static void write(NeuralNetwork neuralNetwork, String filename){
+        neuralNetwork.writeANNToFile(filename);
+        if(DEBUG) System.out.println(neuralNetwork.toString("Neural Network written to file \""+filename+"\""));
+    }
+    public static void print(NeuralNetwork neuralNetwork){
+        System.out.println(neuralNetwork.toString("NeuralNetwork"));
+    }
+    private static void read(NeuralNetwork neuralNetwork, String filename){
+        neuralNetwork.toNeurons(NeuralNetwork.readANNFromFile(filename));
+        if(DEBUG) System.out.println(neuralNetwork.toString("Neural Network read from file \""+filename+"\""));
+    }
+    public static void train(NeuralNetwork neuralNetwork, ArrayList<InputTargetPair> trainingSet, int epochs, double learningRate, double momentum){
+        neuralNetwork.train(epochs, learningRate, trainingSet, momentum);
+        if(DEBUG) System.out.println("Training set error("+trainingSet.size()+"):"+neuralNetwork.getNetworkError());
+    }
+    private static void test(NeuralNetwork neuralNetwork, ArrayList<InputTargetPair> testSet){
+        neuralNetwork.test(testSet);
+        if(DEBUG) System.out.println("Test set error("+testSet.size()+"):"+neuralNetwork.getNetworkError());
+    }
+    private static double[] categorize(NeuralNetwork neuralNetwork, double[] input) {
+        neuralNetwork.feedForward(input);
+        if (DEBUG) System.out.println(neuralNetwork.outputToString());
+        return neuralNetwork.getOutput();
     }
 
 }
